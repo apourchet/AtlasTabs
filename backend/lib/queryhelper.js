87ddb0282a046e7749353afbe3de375f4aff4fcf
Tejas Singh <ts427@cornell.edu>
2014-09-27 17:33:12 -0400

    Should be done. ML though

--- backend/lib/queryhelper.js
+++ backend/lib/queryhelper.js
@@ -66,7 +66,63 @@ QueryHelper.getSuggestedURLs = function(array, lon, lat){
         finalArray.push({url: semiFinalArray[elem].url, sim: (Math.log(num)/(sum/num))})
     }
     finalArray.sort(function(a,b){
-        return a.sim < b.sim
+        return a.sim < b.sim ? 1 : -1
+    })
+    return finalArray.map(function(el){return el.url});
+}
+
+QueryHelper.getPublicURLRanked = function function(array, lon, lat){
+    for (var e in array){
+        var el = array[e].data;
+        array[e].data.distance = QueryHelper.getEuclideanDistance(lon, lat, el.location[0], el.location[1])
+        var thenDate = new Date(array[e].createdAt)
+        array[e].data.timeDifference = QueryHelper.getTimeSimilarity(thenDate.getHours() * 60 + thenDate.getMinutes())
+    };
+    var myArray = []
+    for (var e in array){
+        var elem = Utils.filterUrls(array[e].data.URLs)
+        var td = array[e].data.timeDifference
+        var d = array[e].data.distance
+        var i = array[e].data.id
+        for(var f in elem){
+            myArray.push({url: Utils.cutUrl(elem[f]), timeDifference: td, distance: d, id: i})
+        };
+    };
+    myArray.sort(function(a,b) {
+        return a.url > b.url ? 1 : -1
+    });
+    
+    var semiFinalArray = [] 
+    var u = myArray[0].url
+    var sim = []
+    var temp = []
+    for (var i = 0; i < myArray.length; i++){
+        var s = Math.exp(myArray[0].distance) + Math.exp(myArray[0].timeDifference)
+        if (u === myArray[i].url){
+            sim.push(s)
+            temp.push(myArray[i].id)
+        }
+        else {
+            semiFinalArray.push({url: u, similarities: sim.sort(), numIds: (Utils.removeDuplicates(temp)).length})
+            u = myArray[i].url
+            temp = [myArray[i].id]
+            sim = [s]
+        }
+    };
+    finalArray = []
+    for (var elem in semiFinalArray){
+        var sum = 0
+        var num = 0
+        for (var e in semiFinalArray[elem].similarities){
+            num ++
+            sum += semiFinalArray[elem].similarities[e]
+            //if (num == 10)
+            //    break
+        }
+        finalArray.push({url: semiFinalArray[elem].url, sim: (Math.log(num)*Math.exp(semiFinalArray[elem].numIds)/(sum/num))})
+    }
+    finalArray.sort(function(a,b){
+        return a.sim < b.sim ? 1 : -1
     })
     return finalArray.map(function(el){return el.url});
 }
