e1dc934ddcae0c63f730b60ceb94b3bddc830a15
Tejas Singh <ts427@cornell.edu>
2014-09-27 14:32:35 -0400

    dat machine learning doe

--- backend/lib/queryhelper.js
+++ backend/lib/queryhelper.js
@@ -17,3 +17,58 @@ QueryHelper.getTimeSimilarity = function(mins){
     var diff = Math.abs(curr - mins)
     return diff <= 720 ? diff : (1440 - diff)
 }
+
+QueryHelper.getSuggestedURLs = function(array, lon, lat){
+    for (var e in array){
+        array[e].data.distance = getEuclideanDistance(lon, lat, location[0], location[1])
+        var thenDate = new Date(array[e].createdAt)
+        array[e].data.timeDifference = getTimeSimilarity(thenDate.getHours() * 60 + thenDate.getMinutes())
+    };
+    var myArray = []
+    for (var e in array){
+        var elem = array[e].data.URLs
+        var td = array[e].data.timeDifference
+        var d = array[e].data
+        for(var f in elem){
+            myArray.push({url: elem[f], timeDifference: td, distance: d})
+        };
+    };
+    myArray.sort(function(a,b)){
+        var boo = a.url === b.url
+        if (boo && a.distance == b.distance) return a.timeDifference > b.timeDifference
+        return a.url === b.url ? a.distance > b.distance : a.url > b.url
+    });
+    var semiFinalArray = [] 
+    var u = ""
+    var num = 0
+    var sum = 0
+    for (var e in myArray){
+       if (num == 9) 
+        continue
+       if (myArray[e].url !== u){
+        //stuff
+        if (num == 0)
+            continue
+        semiFinalArray.push({url: u, similarity = Math.log(num)/(sum/num)})
+        num = 0;
+        u = myArray[e].url
+        sum = 0
+       }
+       else{
+        num ++;
+        sum += (Math.exp(myArray[e].distance) * Math.exp(myArray[e].timeDifference))
+       }
+    };
+    semiFinalArray.sort(function(a,b){
+        return a.similarity < b.similarity
+    });
+    var finalArray = []
+    num = 0
+    for(var e in semiFinalArray){
+        finalArray.push(semiFinalArray[e])
+        num++
+        if (num == 5)
+            break
+    };
+    return finalArray
+}
