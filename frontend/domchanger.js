eb9b92be9b2c8e26aa5cdb0540f91f42216e288c
apourchet <antoine.pourchet@gmail.com>
2014-09-27 03:01:42 -0400

    Added a ton of shit

--- frontend/domchanger.js
+++ frontend/domchanger.js
@@ -1,5 +1,24 @@
+var DomChanger = {}
 
+DomChanger.displaySuggestions = function(urls) {
+    console.log("Displaying new suggestions!");
+    var container = document.getElementById("list-container");
+    var items = container.childNodes;
+    var l = items.length;
+    for (var i = 0; i < l; i++) {
+        container.removeChild(items[0]);
+    }
+    for (var i in urls) {
+        var url = urls[i];
+        console.log("Displaying '" + url + "'");
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = url;
+        container.appendChild(newDiv);
+    }
+}
 
 $(document).ready(function(){
-    console.log("Ready!!")
+    console.log("Ready!!");
+    DomChanger.displaySuggestions(["www.google.com", "google2.com", "google3.com"]);
+    setInterval(function() {DomChanger.displaySuggestions([Math.random(), Math.random() + "", Math.random() + "", Math.random() + ""]);}, 4000);
 });
