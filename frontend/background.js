dc3b94af35003c422a411615e5b17a1ec6f1c545
yhyoon12 <Mark.Youngho.Yoon@gmail.com>
2014-09-27 01:13:43 -0400

    Added data gathering function to popup.js. Starting background.js logic.

--- frontend/background.js
+++ frontend/background.js
@@ -1,1 +1,23 @@
 
+chrome.storage.local.clear(function() {
+});
+var currentTabs
+
+function loop() {
+	chrome.tabs.getAllInWindow(null, function(tabs) {
+		currentTabs = tabs
+	});
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position){
+   	lat = position.coords.latitude
+   	lon = position.coords.longitude
+   	})
+}
+
+chrome.runtime.sendMessage({URLs: currentTabs, time: new Date().getTime(), location: {latitude: lat, longitude: lon}, function(response) {
+	});
+
+setInterval(loop, 200);
+setInterval(sendLastData, 50);
--- frontend/popup.js
+++ frontend/popup.js
@@ -3,12 +3,29 @@ var options = {
     lastLocation: {
         latitude: 0,
         longitude: 0
-    }
+    },
+    personal: "yes"
 }
+
+
+document.addEventListener('new_data', function () {
+	console.log("Popup.js called!");
+
+	chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+		console.log("Got a message!");
+
+		data = req.data;
+		var data2 = chrome.storage.sync.get("data")
+		data2.push(data)
+		chrome.storage.sync.set({data: data2}, function() {})
+
+	 	sendResponse({ok: 1});
+	});
+});
 
 /* Returns a list of urls that you should open*/
 function getTabSuggestions() {
-    
+
 }
 
 /* Uses chrome.tabs to open all of the tabs from the suggestions*/
