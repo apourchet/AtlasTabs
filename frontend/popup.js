5dc92a843bca50c9d44bbe8c2a43d5dc736e0a50
apourchet <antoine.pourchet@gmail.com>
2014-09-26 22:04:24 -0400

    Added some stubs

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,1 +1,19 @@
+var options = {
+    
+}
+
+/* Returns a list of urls that you should open*/
+function getTabSuggestions() {
+    
+}
+
+/* Uses chrome.tabs to open all of the tabs from the suggestions*/
+function openTabSuggestions(tabs) {
+    
+}
+
+/* Changes the suggestion options. */
+function setSuggestionOptions(options) {
+    
+}
 
