92cc653e129f33350df99288b9b4b7b4cfaeb15a
yhyoon12 <Mark.Youngho.Yoon@gmail.com>
2014-09-28 01:02:28 -0400

    Added thigns

--- frontend/popup.js
+++ frontend/popup.js
@@ -109,3 +109,17 @@ $(document).ready(function(){
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                DomChanger.openTab(location);
+                // chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
